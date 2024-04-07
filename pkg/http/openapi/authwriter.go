396973c18187dc6a4d7329873d41516bb4fb7d4a
hsinhoyeh <yhh92u@gmail.com>
2024-04-07 15:01:58 +0800

    connect with openapi

---
+++ pkg/http/openapi/authwriter.go
@@ -0,0 +1,32 @@
+package openapi
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
+
+	"github.com/sdinsure/agent/pkg/errors"
+	sdinsureruntime "github.com/sdinsure/agent/pkg/grpc/server/runtime"
+)
+
+type ContextClientAuthInfoWriter struct {
+	ctx context.Context
+}
+
+func NewContextClientAuthInfoWriter(ctx context.Context) *ContextClientAuthInfoWriter {
+	return &ContextClientAuthInfoWriter{ctx}
+}
+
+var (
+	_ runtime.ClientAuthInfoWriter = &ContextClientAuthInfoWriter{}
+)
+
+func (c *ContextClientAuthInfoWriter) AuthenticateRequest(clientRequest runtime.ClientRequest, registry strfmt.Registry) error {
+	keyVal, foundKey := sdinsureruntime.KeyInfo(c.ctx)
+	if !foundKey {
+		return errors.NewInvalidAuth(errors.New("auth: keyinfo not found"))
+	}
+	return clientRequest.SetHeaderParam("Authorization", fmt.Sprintf("Bearer %s", keyVal))
+}
